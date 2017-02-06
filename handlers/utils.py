0f00bd4a0cd4d86d0fcb4bfdd9a9746269071f88
iamareebjamal <jamal.areeb@gmail.com>
2017-02-06 23:47:14 +0530

    refactor: Make item access method common among classes

--- handlers/utils.py
+++ handlers/utils.py
@@ -18,79 +18,14 @@ def verify_page(doc):
     return data
 
 
-def get_complete_attendance(user, use_stored=True):
-    from CompleteAttendance import CompleteAttendance
-    key = 'complete_attendance'
+def get_item(key, user, cached, get_method, *args):
     data = memcache.get(key)
 
-    if data is None or not use_stored:
-        store_data = CacheData.get_by_id(key)
-        if store_data is None or use_stored:
-            data = CompleteAttendance.get_attendance_by_fetch()
-            store_data = CacheData(id=key)
-            store_data.request = key
-            store_data.data = json.dumps(data)
-            store_data.put()
-        else:
-            data = json.loads(store_data.data)
-        data['user'] = user.username
-        memcache.set(key, data)
-
-    return data
-
-
-def get_course_attendance(course, user, use_stored=True):
-    from CompleteAttendance import ClassAttendance
-    key = 'course_' + course
-    data = memcache.get(key)
-
-    if data is None or not use_stored:
-        store_data = CacheData.get_by_id(key)
-        if store_data is None or not use_stored:
-            data = ClassAttendance.get_attendance_by_fetch(course)
-            store_data = CacheData(id=key)
-            store_data.request = key
-            store_data.data = json.dumps(data)
-            store_data.put()
-        else:
-            data = json.loads(store_data.data)
-        data['user'] = user.username
-        memcache.set(key, data)
-
-    return data
-
-
-def get_result(fac_no, enrol_no, user, use_stored=True):
-    from Result import Result
-    key = 'result_' + fac_no.upper() + ':' + enrol_no.upper()
-    data = memcache.get(key)
-
-    if data is None or not use_stored:
-        store_data = CacheData.get_by_id(key)
-        if store_data is None or not use_stored:
-            data = Result.get_result_by_fetch(fac_no, enrol_no)
-            store_data = CacheData(id=key)
-            store_data.request = key
-            store_data.data = json.dumps(data)
-            store_data.put()
-        else:
-            data = json.loads(store_data.data)
-        data['user'] = user.username
-        memcache.set(key, data)
-
-    return data
-
-
-def get_attendance(fac_no, user, use_stored=True):
-    from Attendance import Attendance
-    key = 'attendance_' + fac_no.upper()
-    data = memcache.get(key)
-
-    if data is None or data['error'] or not use_stored:
+    if data is None or data['error'] or not cached:
         store_data = CacheData.get_by_id(key)
 
-        if store_data is None or json.loads(store_data.data)['error'] or not use_stored:
-            data = Attendance.get_attendance_by_fetch(fac_no)
+        if store_data is None or json.loads(store_data.data)['error'] or not cached:
+            data = get_method(*args)
             if not data['error']:
                 store_data = CacheData(id=key)
                 store_data.request = key
@@ -98,9 +33,32 @@ def get_attendance(fac_no, user, use_stored=True):
                 store_data.put()
         else:
             data = json.loads(store_data.data)
-        data['fac'] = fac_no.upper()
         data['user'] = user.username
         memcache.set(key, data)
 
     return data
+
+
+def get_complete_attendance(user, use_stored=True):
+    from CompleteAttendance import CompleteAttendance
+    key = 'complete_attendance'
+    return get_item(key, user, use_stored, CompleteAttendance.get_attendance_by_fetch)
+
+
+def get_course_attendance(course, user, use_stored=True):
+    from CompleteAttendance import ClassAttendance
+    key = 'course_' + course
+    return get_item(key, user, use_stored, ClassAttendance.get_attendance_by_fetch, course)
+
+
+def get_result(fac_no, enrol_no, user, use_stored=True):
+    from Result import Result
+    key = 'result_' + fac_no.upper() + ':' + enrol_no.upper()
+    return get_item(key, user, use_stored, Result.get_result_by_fetch, fac_no, enrol_no)
+
+
+def get_attendance(fac_no, user, use_stored=True):
+    from Attendance import Attendance
+    key = 'attendance_' + fac_no.upper()
+    return get_item(key, user, use_stored, Attendance.get_attendance_by_fetch, fac_no)
 
